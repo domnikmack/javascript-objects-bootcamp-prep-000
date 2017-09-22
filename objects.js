0c64b0c3b2ccc653f1ec628b3004c79128fab06e
Domnik Mack <domnikmack@gmail.com>
2017-09-22 03:01:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,1 @@
-var playlist = { radiohead: 'There there' };
-
-function updatePlaylist (playlist, artistName, songTitle) {
-  playlist.artist = song;
-  return playlist;
-}
-
-funtion removeFromPlaylist (playlist, artistName) {
-  delete playlist.artist;
-  return playlist;
-}
+var playlist = {}
